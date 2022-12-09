8c6676148fd8fdf6f0345679d45f88d907bfdc90
vWhitex <120167873+vWhitex@users.noreply.github.com>
2022-12-09 10:31:06 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,9 +1,11 @@
 function checkTemp(p1){
+    // hey timo :D
+
     if(p1<20){
-        console.log("-1")
+        return "-1"
     } else if(p1<40){
-        console.log("0")
+        return "0"
     } else {
-        console.log("1")
+        return "1"
     }
 }
